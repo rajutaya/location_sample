e03ae8b859b79ce3ab9c8c6f9323a2c54fdaf332
rajutaya <rajutaya@gmail.com>
2019-05-08 13:23:04 +0530

    Update application_controller.rb
    Required params added for API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  require 'params_missing'
   protect_from_forgery with: :exception
+
+   rescue_from ParamsMissing do |exception|
+    render json: {status: exception.status, message: exception.info}
+  end
+
+  def required_params(params, *arg_list)
+    blanks = arg_list - arg_list.reject {|x| params[x] == '' or params[x].nil? }
+    raise ParamsMissing.new(info: "blank or missing params: #{blanks.inspect}", status: 412) if blanks.present?
+    parameters = arg_list.map {|x| params[x] }
+    parameters.count == 1 ? parameters.first : parameters
+  end
 end
