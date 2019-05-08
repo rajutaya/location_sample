48b7de4bd876c0a8ebcaa3a7e954ed4926431927
rajutaya <rajutaya@gmail.com>
2019-05-08 13:22:09 +0530

    Update routes.rb
    Search API added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root :to =>'locations#index', :as => :dashboard
-  resources :locations
+  resources :locations do
+  	get 'search', on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
